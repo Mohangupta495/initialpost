00b3901e7525221e2bb481fbb23d7627c2f0ab1a
Mohan Gupta <75502789+Mohangupta495@users.noreply.github.com>
2023-02-12 10:44:26 +0530

    updated api file

--- index.js
+++ index.js
@@ -1,35 +1,74 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
 
 const app = express();
 const port = 8000;
-
-// Where we will keep books
-let books = [
-  { name: "mohan", description: "this is a book", author: "John" },
-  { name: "mohan", description: "this is a book", author: "John" },
-  { name: "mohan", description: "this is a book", author: "John" },
-  { name: "mohan", description: "this is a book", author: "John" },
-  { name: "mohan", description: "this is a book", author: "John" },
-  { name: "mohan", description: "this is a book", author: "John" },
-  { name: "mohan", description: "this is a book", author: "John" },
-];
-
+const bookSchema = {
+  name: String,
+  author: String,
+  price: String,
+};
+const Book = mongoose.model("bookStore", bookSchema);
+let books = [];
 app.use(cors());
-
-// Configuring body parser middleware
+app.set("view engine", "ejs");
+mongoose.connect(
+  "mongodb+srv://mohan:mohan@cluster0.ox3vuuo.mongodb.net/bookStore?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/book", (req, res) => {
+app.get("/book", async (req, res) => {
+  const books = await Book.find();
   res.send(books);
 });
-
+app.post("/addbook", (req, res) => {
+  const bookDetails = req.body;
+  books.push(bookDetails);
+  const book = new Book(bookDetails);
+  book.save(function (err) {
+    if (err) {
+      res.send(err);
+      console.log(err);
+    } else {
+      res.send(book);
+      console.log("added succs");
+    }
+  });
+});
+app.post("/deletebook", (req, res) => {
+  const bookDetails = req.body;
+  const book = Book.findByIdAndDelete(bookDetails._id);
+  res.send(book);
+});
+app.post("/update", (req, res) => {
+  const bookDetails = req.body;
+  console.log(bookDetails);
+  const dbResponse = Book.findByIdAndUpdate(bookDetails._id, {
+    name: bookDetails.name,
+    author: bookDetails.name,
+    price: bookDetails.name,
+  });
+  console.log(dbResponse);
+  res.send(dbResponse);
+});
+app.post("/getbook", async (req, res) => {
+  const bookDetails = req.body;
+  const book = await Book.findById(bookDetails._id);
+  res.send(book);
+});
 app.get("/", (req, res) => {
-  res.send("Hey this is my APdjffjdlj nowfdjfjdlsfjdsfljI running 🥳");
+  res.send("Hey this is my API");
 });
 app.listen(port, () =>
   console.log(`Hello world app listening on port ${port}!`)
 );
+
 module.exports = app;
