0688e6c2ed7aa6c70a8456c87665f1e7cfe4e20a
Mohan Gupta <75502789+Mohangupta495@users.noreply.github.com>
2023-02-25 23:36:12 +0530

    final cors update

--- index.js
+++ index.js
@@ -6,22 +6,26 @@ const ejs = require("ejs");
 
 const app = express();
 const port = 8000;
-const bookSchema = {
-  name: String,
-  author: String,
-  price: String,
-};
 const mailSchema = {
   name: String,
   desc: String,
   phone: String,
   email: String,
 };
-const Book = mongoose.model("bookStore", bookSchema);
 const Mail = mongoose.model("mailStore", mailSchema);
-let books = [];
-app.use(cors({ origin: "https://webbrings.com/" }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "Content-Type",
+    "Authorization"
+  );
+  next();
+});
+
 app.set("view engine", "ejs");
+
 mongoose.connect(
   "mongodb+srv://mohan:mohan@cluster0.ox3vuuo.mongodb.net/bookStore?retryWrites=true&w=majority",
   {
@@ -31,49 +35,7 @@ mongoose.connect(
 );
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.get("/book", async (req, res) => {
-  const books = await Book.find();
-  res.send(books);
-});
-app.post("/addbook", (req, res) => {
-  const bookDetails = req.body;
-  books.push(bookDetails);
-  const book = new Book(bookDetails);
-  book.save(function (err) {
-    if (err) {
-      res.send(err);
-      console.log(err);
-    } else {
-      res.send(book);
-      console.log("added succs");
-    }
-  });
-});
-app.post("/deletebook", (req, res) => {
-  const bookDetails = req.body;
-  const book = Book.findByIdAndDelete(bookDetails._id);
-  res.send(book);
-});
-app.post("/update", (req, res) => {
-  const bookDetails = req.body;
-  console.log(bookDetails);
-  const dbResponse = Book.findByIdAndUpdate(bookDetails._id, {
-    name: bookDetails.name,
-    author: bookDetails.name,
-    price: bookDetails.name,
-  });
-  console.log(dbResponse);
-  res.send(dbResponse);
-});
-app.post("/getbook", async (req, res) => {
-  const bookDetails = req.body;
-  const book = await Book.findById(bookDetails._id);
-  res.send(book);
-});
 app.post("/sendmail", (req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  console.log("data", req.body);
   const data = new Mail({
     name: req.body.name,
     email: req.body.email,
