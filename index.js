3dbc4f9ccc81cf7c5aa9f4264f362de11611a0ba
Mohan Gupta <75502789+Mohangupta495@users.noreply.github.com>
2023-02-27 00:00:39 +0530

    remove dotenv

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require("express");
-const dotenv = require("dotenv");
 const connectionDB = require("./srcc/database/mongoconnection.handler");
 const cors = require("cors");
 const visiterDetails = require("./srcc/models/visiter/controller/visiter.controller");
 
 const app = express();
 const router = express.Router();
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
 app.use(cors());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -31,7 +27,7 @@ app.use("/", router.post("/visiter/signUp", visiterDetails));
 app.get("/", (req, res) => {
   res.send("Welcome to WebBrings api section.");
 });
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- srcc/database/mongoconnection.handler.js
+++ srcc/database/mongoconnection.handler.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
 const connectionDB = async () => {
+  const MONGODB_URI =
+    "mongodb+srv://mohan:mohan@webbrings.pageubo.mongodb.net/retryWrites=true&w=majority";
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    const conn = await mongoose.connect(MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
